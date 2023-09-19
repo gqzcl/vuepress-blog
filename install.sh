e6456961b08aecfcb319ac3d364a907376317c7e
gqzcl <gqzcl@qq.com>
2023-09-20 00:23:06 +0800

    feat: add install bash

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# This script will install Node.js 18.18 on Ubuntu
+# Check the official website for the latest version: https://nodejs.org/en/download/
+
+# Download the Node.js 18.18 binary package
+yarn config set ignore-engines true
+
+yarn install
